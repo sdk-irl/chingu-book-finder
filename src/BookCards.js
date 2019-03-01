b6b50ebc44f507aab873ef48aadae2b8fb510127
Susanna Kline <susannakline@gmail.com>
2019-02-28 22:28:44 -0600

    Start api call structure

--- src/BookCards.js
+++ src/BookCards.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 
-class BookCards extends Component {
-    render() {
-        return (
-            <div >
-                <h2>my book cards component is showing up</h2>
-            </div>
-        );
+//stateless component
 
-    }
+const bookCards = ( props ) => {
+    //component returns
+    //book image
+    //book title
+    //book author
+    //book publisher
+    //link to more information
+    return (
+        <div id="results">
+            <h2></h2>
+        </div>
+    );
 }
 
-export default BookCards;
+export default bookCards;
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 
+const API_KEY = "AIzaSyA8mV96_G2-uDhDT5EfxxYowv0wima9Jwg";
+// what is this? <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jwuery.min.js"></script>
+let googBooksUrl = "https://www.googleapis.com/books/v1/volumes/s1gVAAAAYAAJ";
+
 class SearchBar extends Component {
+    state = {
+
+    }
+    updateSearch = value => {
+        let request = new Request (googBooksUrl, {
+            method: "GET",
+            dataType: "json"
+        });
+    }
+              
     render() {
         return (
             <div className="Search-bar">
+                <input
+                    type="text"
+                    placeholder="Search by title"
+                    //set value
+
+                    //set value on change 
+
+                />
                 <button className="Button">Search</button>
             </div>
         )
