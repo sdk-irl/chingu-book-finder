71ec543df53f7bd469e8832b3f98f20877fb76d5
Susanna Kline <susannakline@gmail.com>
2019-03-04 20:35:54 -0600

    Updates

---
+++ precache-manifest.f67ce86c83dfcb6e3d2f44333a44129f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8f03a688b07d56d6741",
+    "url": "/chingu-book-finder/static/js/runtime~main.d8f03a68.js"
+  },
+  {
+    "revision": "16c7af54dd798a20033b",
+    "url": "/chingu-book-finder/static/js/main.16c7af54.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/chingu-book-finder/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "16c7af54dd798a20033b",
+    "url": "/chingu-book-finder/static/css/main.8c0064cb.chunk.css"
+  },
+  {
+    "revision": "39a43ddc6b3210d9d430b523beec67d6",
+    "url": "/chingu-book-finder/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/chingu-book-finder/precache-manifest.f67ce86c83dfcb6e3d2f44333a44129f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/chingu-book-finder/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
